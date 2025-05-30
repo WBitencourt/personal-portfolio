9b51b57c445196677af2b041163ea91406deb228
Wendell Bitencourt <bat.delgeorge@hotmail.com>
2025-05-30 18:28:30 -0300

    fix: profile github icon

---
+++ src/app/[locale]/fall-back-icon.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { UserCircleIcon } from '@phosphor-icons/react';
+
+export default function FallbackIcon() {
+  return (
+    <UserCircleIcon 
+      alt="Wendell Bitencourt"
+      className="rounded-full w-[200px] h-[200px]" 
+      weight="thin" 
+    />
+  );
+}
