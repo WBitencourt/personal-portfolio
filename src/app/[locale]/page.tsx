4c52ca56cea313089aa1fff579c421fbc13dc9f4
Wendell Bitencourt <bat.delgeorge@hotmail.com>
2025-09-29 17:32:34 -0300

    fix: layout at session 'main languages and tools'

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -108,7 +108,7 @@ export default async function Home() {
                   <h3 className="text-lg font-semibold text-slate-500 dark:text-gray-400 mb-3">
                     {t('skills.general.title')}
                   </h3>
-                  <div className="grid grid- grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
+                  <div className="grid grid-cols-8 md:grid-cols-10 lg:grid-cols-4 gap-3">
                     {(t.raw('skills.general.techs') as Array<{name: string, src: string, url: string}>).map((tech, index) => (
                       <TechCard
                         key={index}
@@ -126,7 +126,7 @@ export default async function Home() {
                   <h3 className="text-lg font-semibold text-slate-500 dark:text-gray-400 mb-3">
                     {t('skills.frontend.title')}
                   </h3>
-                  <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
+                  <div className="grid grid-cols-8 md:grid-cols-10 lg:grid-cols-4 gap-3">
                     {(t.raw('skills.frontend.techs') as Array<{name: string, src: string, url: string}>).map((tech, index) => (
                       <TechCard
                         key={index}
@@ -144,7 +144,7 @@ export default async function Home() {
                   <h3 className="text-lg font-semibold text-slate-500 dark:text-gray-400 mb-3">
                     {t('skills.backend_devops.title')}
                   </h3>
-                  <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
+                  <div className="grid grid-cols-8 md:grid-cols-10 lg:grid-cols-4 gap-3">
                     {(t.raw('skills.backend_devops.techs') as Array<{name: string, src: string, url: string}>).map((tech, index) => (
                       <TechCard
                         key={index}
@@ -162,7 +162,7 @@ export default async function Home() {
                   <h3 className="text-lg font-semibold text-slate-500 dark:text-gray-400 mb-3">
                     {t('skills.aws.title')}
                   </h3>
-                  <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
+                  <div className="grid grid-cols-8 md:grid-cols-10 lg:grid-cols-4 gap-3">
                     {(t.raw('skills.aws.techs') as Array<{name: string, src: string, url: string}>).map((tech, index) => (
                       <TechCard
                         key={index}
@@ -180,7 +180,7 @@ export default async function Home() {
                   <h3 className="text-lg font-semibold text-slate-500 dark:text-gray-400 mb-3">
                     {t('skills.worked_with.title')}
                   </h3>
-                  <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
+                  <div className="grid grid-cols-8 md:grid-cols-10 lg:grid-cols-4 gap-3">
                     {(t.raw('skills.worked_with.techs') as Array<{name: string, src: string, url: string}>).map((tech, index) => (
                       <TechCard
                         key={index}
@@ -198,7 +198,7 @@ export default async function Home() {
                   <h3 className="text-lg font-semibold text-slate-500 dark:text-gray-400 mb-3">
                     {t('skills.studying.title')}
                   </h3>
-                  <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
+                  <div className="grid grid-cols-8 md:grid-cols-10 lg:grid-cols-4 gap-3">
                     {(t.raw('skills.studying.techs') as Array<{name: string, src: string, url: string}>).map((tech, index) => (
                       <TechCard
                         key={index}
@@ -216,7 +216,7 @@ export default async function Home() {
                   <h3 className="text-lg font-semibold text-slate-500 dark:text-gray-400 mb-3">
                     {t('skills.interested.title')}
                   </h3>
-                  <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3">
+                  <div className="grid grid-cols-8 md:grid-cols-10 lg:grid-cols-4 gap-3">
                     {(t.raw('skills.interested.techs') as Array<{name: string, src: string, url: string}>).map((tech, index) => (
                       <TechCard
                         key={index}
